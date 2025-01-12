ce9f180cfe49592d585f79565f2587649f4c1743
Tiago S.C. <72054311+Tiago-Silverio-da-Costa@users.noreply.github.com>
2025-01-12 14:34:15 -0300

    feat: header base

---
+++ components/Header.vue
@@ -0,0 +1,15 @@
+
+
+<template>
+    <header className="bg-[#181C14]">
+        <div className="flex items-center justify-between gap-2 py-4 w-5/6 max-w-4xl mx-auto text-white">
+            <NuxtImg src="/logo-no-name.svg" alt="image" width="40" height="40" />
+            <NuxtImg src="/logo-name.svg" alt="image" width="200" height="200" />
+            <div>button</div>
+            <div className="block md:hidden">
+                <NuxtImg src="/menuIcons.svg" alt="image" width="40" height="40" />
+            </div>
+        </div>
+
+    </header>
+</template>
