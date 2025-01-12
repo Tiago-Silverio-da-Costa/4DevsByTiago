28fb2dd06b924589fbcec469d252b4c5499f9f06
Tiago S.C. <72054311+Tiago-Silverio-da-Costa@users.noreply.github.com>
2025-01-12 19:54:20 -0300

    feat: login page

--- components/Header.vue
+++ components/Header.vue
@@ -4,8 +4,8 @@
             <NuxtImg src="/logo-no-name.svg" alt="image" width="40" height="40" />
             <NuxtImg src="/logo-name.svg" alt="image" width="200" height="200" />
             <div class="md:flex items-center gap-2 hidden ">
-                <button class="px-4 py-2 rounded-md bg-[#FF8200] font-bold">Log in</button>
-                <button class="px-4 py-2 rounded-md bg-[#FF8200] font-bold">Sign up</button>
+                <NuxtLink :to="`/login`" class="px-4 py-2 rounded-md bg-[#FF8200] font-bold">Log in</NuxtLink>
+                <NuxtLink :to="`/register`" class="px-4 py-2 rounded-md bg-[#FF8200] font-bold">Sign up</NuxtLink>
             </div>
             <div class="block md:hidden">
                 <NuxtImg src="/menuIcons.svg" alt="image" width="40" height="40" />
---
+++ components/Login.vue
@@ -0,0 +1,13 @@
+<template>
+    <section class="bg-[#181C14]">
+        <div class=" flex flex-col justify-center items-center gap-4 mx-auto w-5/6 max-w-xs py-8 h-screen">
+            <NuxtImg src="/logo-no-name.svg" alt="image" width="100" height="200" />
+            <h1 class="text-white text-3xl font-bold">Login</h1>
+            <form class="w-full flex flex-col justify-center items-center gap-4 mt-8">
+                <input class="w-full placeholder:pl-4" placeholder="email" />
+                <input class="w-full placeholder:pl-4" placeholder="password" />
+                <button class="w-full px-4 py-2 rounded-md bg-[#FF8200] font-bold">Entrar</button>
+            </form>
+        </div>
+    </section>
+</template>
