8d9dad7d89f7a1735de871d25a6c08d18b7c5853
Tiago S.C. <72054311+Tiago-Silverio-da-Costa@users.noreply.github.com>
2025-01-12 20:02:10 -0300

    feat: register page

--- components/Register.vue
+++ components/Register.vue
@@ -2,13 +2,13 @@
     <section class="bg-[#181C14]">
         <div class=" flex flex-col justify-center items-center gap-4 mx-auto w-5/6 max-w-xs py-8 h-screen">
             <NuxtImg src="/logo-no-name.svg" alt="image" width="100" height="200" />
-            <h1 class="text-white text-3xl font-bold">Login</h1>
+            <h1 class="text-white text-3xl font-bold">Register</h1>
             <form class="w-full flex flex-col justify-center items-center mt-8">
+                <input class="w-full placeholder:pl-4 mt-4" placeholder="username" />
                 <input class="w-full placeholder:pl-4 mt-4" placeholder="email" />
                 <input class="w-full placeholder:pl-4 mt-4" placeholder="password" />
-                <NuxtLink :to="`/forgotpassword`" class="text-[#FF8200] font-semibold text-sm mt-2">forgot password?</NuxtLink>
                 <button class="w-full px-4 py-2 rounded-md bg-[#FF8200] font-bold mt-4">Entrar</button>
-                <NuxtLink :to="`/register`" class="text-[#FF8200] font-semibold text-sm mt-2">don't have an account?</NuxtLink>
+                <NuxtLink :to="`/login`" class="text-[#FF8200] font-semibold text-sm mt-2">already have an account?</NuxtLink>
             </form>
         </div>
     </section>
