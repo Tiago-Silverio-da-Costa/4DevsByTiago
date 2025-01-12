ec3a3af69b9b8c93d70a768036ec56b9e1a99826
Tiago S.C. <72054311+Tiago-Silverio-da-Costa@users.noreply.github.com>
2025-01-12 15:30:26 -0300

    feat: footer base + header buttons added + tailwind import fixed

---
+++ components/Footer.vue
@@ -0,0 +1,7 @@
+<template>
+    <footer class="bg-[#181C14]">
+        <div class="flex justify-center py-4 w-5/6 max-w-4xl mx-auto text-white">
+            <p class="font-semibold">4DevsByTiagoSC | Todos os direitos reservados</p>
+        </div>
+    </footer>
+</template>
--- components/Header.vue
+++ components/Header.vue
@@ -1,12 +1,13 @@
-
-
 <template>
-    <header className="bg-[#181C14]">
-        <div className="flex items-center justify-between gap-2 py-4 w-5/6 max-w-4xl mx-auto text-white">
+    <header class="bg-[#181C14]">
+        <div class="flex md:row items-center justify-between gap-2 py-4 w-5/6 max-w-4xl mx-auto text-white">
             <NuxtImg src="/logo-no-name.svg" alt="image" width="40" height="40" />
             <NuxtImg src="/logo-name.svg" alt="image" width="200" height="200" />
-            <div>button</div>
-            <div className="block md:hidden">
+            <div class="md:flex items-center gap-2 hidden ">
+                <button class="px-4 py-2 rounded-md bg-[#FF8200] font-bold">Log in</button>
+                <button class="px-4 py-2 rounded-md bg-[#FF8200] font-bold">Sign up</button>
+            </div>
+            <div class="block md:hidden">
                 <NuxtImg src="/menuIcons.svg" alt="image" width="40" height="40" />
             </div>
         </div>
