641845e601366178ea6a0df2471a6fb4a7e11cbc
Tiago S.C. <72054311+Tiago-Silverio-da-Costa@users.noreply.github.com>
2025-01-14 20:46:24 -0300

    feat: get post by id

--- pages/[id].vue
+++ pages/[id].vue
@@ -2,66 +2,45 @@
     <div v-if="post" class="bg-[#3C3D37] text-white">
         <div class="mx-auto w-5/6 max-w-4xl py-4">
             <NuxtImg v-if="post.post_image_url" :src="post.post_image_url" alt="image" width="100%" height="200" />
+            <!-- <span class="font-bold text-xl mt-2">Autor: {{ post.author_name }}</span> -->
             <h1 class="font-bold text-4xl mt-4">{{ post.title }}</h1>
             <p class="mt-4">{{ post.content }}</p>
             <div class="mt-4 text-[#FF8200]">
-                <span>{{ post.publication_date }}</span> | <span>{{ post.category }}</span>
+                <span>{{ formattedPublicationDate }}</span> | <span>{{ post.category_name }}</span>
             </div>
         </div>
-    </div>
-    <div v-else>
-        <p class="text-center text-white">Post não encontrado.</p>
     </div>
 </template>
 
 
 <script>
+import axios from 'axios'
+
 export default {
     data() {
         return {
-            post: null,
+            post: [],
         };
     },
     created() {
         const id = Number(this.$route.params.id);
-        const posts = [
-            {
-                id: 1,
-                title: 'Dicas para Desenvolvedores',
-                description: 'Descubra como novas ferramentas e frameworks estão revolucionando o desenvolvimento web e ajudando a criar soluções mais eficientes.',
-                content: 'Descubra como novas ferramentas e frameworks estão revolucionando o desenvolvimento web e ajudando a criar soluções mais eficientes. Descubra como novas ferramentas e frameworks estão revolucionando o desenvolvimento web e ajudando a criar soluções mais eficientes. Descubra como novas ferramentas e frameworks estão revolucionando o desenvolvimento web e ajudando a criar soluções mais eficientes. Descubra como novas ferramentas e frameworks estão revolucionando o desenvolvimento web e ajudando a criar soluções mais eficientes.',
-                post_image_url: 'https://i.imgur.com/4WsRhkB.jpeg',
-                publication_date: '2025-01-12',
-                category: 'Tecnologia',
-            },
-            {
-                id: 2,
-                title: 'Dicas para Desenvolvedores',
-                description: 'Descubra como novas ferramentas e frameworks estão revolucionando o desenvolvimento web e ajudando a criar soluções mais eficientes.',
-                content: 'Descubra como novas ferramentas e frameworks estão revolucionando o desenvolvimento web e ajudando a criar soluções mais eficientes. Descubra como novas ferramentas e frameworks estão revolucionando o desenvolvimento web e ajudando a criar soluções mais eficientes. Descubra como novas ferramentas e frameworks estão revolucionando o desenvolvimento web e ajudando a criar soluções mais eficientes. Descubra como novas ferramentas e frameworks estão revolucionando o desenvolvimento web e ajudando a criar soluções mais eficientes.',
-                post_image_url: 'https://i.imgur.com/4WsRhkB.jpeg',
-                publication_date: '2025-01-10',
-                category: 'Desenvolvimento',
-            },
-            {
-                id: 3,
-                title: 'Tendências de Design 2025',
-                description: 'Saiba o que está em alta no design este ano e como aplicar essas tendências para criar interfaces modernas e intuitivas.',
-                content: 'Descubra como novas ferramentas e frameworks estão revolucionando o desenvolvimento web e ajudando a criar soluções mais eficientes. Descubra como novas ferramentas e frameworks estão revolucionando o desenvolvimento web e ajudando a criar soluções mais eficientes. Descubra como novas ferramentas e frameworks estão revolucionando o desenvolvimento web e ajudando a criar soluções mais eficientes. Descubra como novas ferramentas e frameworks estão revolucionando o desenvolvimento web e ajudando a criar soluções mais eficientes.',
-                post_image_url: 'https://i.imgur.com/4WsRhkB.jpeg',
-                publication_date: '2025-01-08',
-                category: 'Design',
-            },
-        ];
 
-        this.post = posts.find((p) => p.id === id);
+        axios.get(`http://127.0.0.1:8080/api/posts/${id}`)
+            .then(response => {
+                console.log("Posts fetched:", response.data)
+                this.post = response.data;
 
-        if (!this.post) {
-            console.error("Post não encontrado!");
-        } else {
-            console.log("Post encontrado:", this.post);
-        }
-
+                const publicationDate = new Date(response.data.publication_date)
+                this.formattedPublicationDate = publicationDate.toLocaleDateString("Pt-BR", {
+                    timeZone: "America/Sao_Paulo",
+                    year: "numeric",
+                    month: "2-digit",
+                    day: "2-digit",
+                });
+            })
+            .catch(error => {
+                console.error("Error fetching posts", error)
+            })
     },
 };
 </script>
