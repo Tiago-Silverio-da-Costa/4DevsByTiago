f413be2aa7b5f0fa82159e8e1e95dcb49115dd47
Tiago S.C. <72054311+Tiago-Silverio-da-Costa@users.noreply.github.com>
2025-01-21 21:20:43 -0300

    feat: content system

--- components/HomePage.vue
+++ components/HomePage.vue
@@ -2,37 +2,37 @@
     <div class="bg-[#3C3D37] text-white">
         <div class="mx-auto w-5/6 max-w-[49.5rem] py-4">
             <Search @search="updateSearchQuery" :intialQuery="searchQuery" />
+            <div :class="{ 'min-h-[100vh]': searchQuery.length > 0 }">
+                <div class="flex flex-wrap gap-5 justify-center items-stretch w-full">
 
-            <div class="flex flex-wrap gap-5 justify-center w-full"
-                :class="{ 'min-h-[100vh]': searchQuery.length > 0 }">
-
-                <div v-if="searchQuery && filteredPosts.length === 0"
-                    class="flex justify-center self-center items-center text-center text-xl font-bold bg-[#FF8200] text-white px-8 py-4 h-fit rounded-md mt-8">
-                    Nenhum resultado encontrado.
-                </div>
-
-
-                <NuxtLink :to="`/${post.id}`" v-for="post in filteredPosts" :key="post.id"
-                    class="bg-[#181C14] mt-4 w-96 border-2 border-[#FF8200] rounded-md overflow-hidden">
-                    <!-- image -->
-                    <div class="flex-1 bg-[#181C14] border-b-2 border-b-[#FF8200]">
-                        <NuxtImg :src="post.post_image_url" alt="image" width="400" height="200" />
+                    <div v-if="searchQuery && filteredPosts.length === 0"
+                        class="flex justify-center self-center items-center text-center text-xl font-bold bg-[#FF8200] text-white px-8 py-4 h-fit rounded-md mt-8">
+                        Nenhum resultado encontrado.
                     </div>
-                    <!-- body -->
-                    <div class="flex flex-col gap-4 px-8 py-4">
-                        <h2 class="font-bold text-2xl"> {{ post.title }}</h2>
-                        <p class=" text-md flex max-w-md ">{{ post.description }}</p>
-                    </div>
-                    <!-- footer -->
-                    <div
-                        class="flex px-8 py-4 items-center justify-between border-t-2 border-t-[#FF8200] text-[#FF8200]">
-                        <div class="flex items-center gap-2">
-                            <NuxtImg src="watchIcon.svg" alt="image" width="20" height="20" />
-                            <span>{{ post.publication_date }}</span>
+
+
+                    <NuxtLink :to="`/${post.id}`" v-for="post in filteredPosts" :key="post.id"
+                        class="bg-[#181C14] mt-4 w-96 border-2 border-[#FF8200] rounded-md overflow-hidden">
+                        <!-- image -->
+                        <div class="flex-1 bg-[#181C14] border-b-2 border-b-[#FF8200]">
+                            <NuxtImg :src="post.post_image_url" alt="image" width="400" height="200" />
                         </div>
-                        <span>{{ post.category }}</span>
-                    </div>
-                </NuxtLink>
+                        <!-- body -->
+                        <div class="flex flex-col gap-4 px-8 py-4">
+                            <h2 class="font-bold text-2xl"> {{ post.title }}</h2>
+                            <p class=" text-md flex max-w-md ">{{ post.description }}</p>
+                        </div>
+                        <!-- footer -->
+                        <div
+                            class="flex px-8 py-4 items-center justify-between border-t-2 border-t-[#FF8200] text-[#FF8200]">
+                            <div class="flex items-center gap-2">
+                                <NuxtImg src="watchIcon.svg" alt="image" width="20" height="20" />
+                                <span>{{ post.publication_date }}</span>
+                            </div>
+                            <span>{{ post.category }}</span>
+                        </div>
+                    </NuxtLink>
+                </div>
             </div>
         </div>
     </div>
--- pages/[id].vue
+++ pages/[id].vue
@@ -4,7 +4,13 @@
             <NuxtImg v-if="post.post_image_url" :src="post.post_image_url" alt="image" width="100%" height="200" />
             <!-- <span class="font-bold text-xl mt-2">Autor: {{ post.author_name }}</span> -->
             <h1 class="font-bold text-4xl mt-4">{{ post.title }}</h1>
-            <p class="mt-4">{{ post.content }}</p>
+            <div class="mt-4" v-html="introductionContent"></div>
+            <ul class="mt-4 list-disc list-inside text-[#FF8200]" v-if="summary.length">
+                <li v-for="(item, index) in summary" :key="index">
+                    <nuxt-link :to="`#${item.formattedTitle}`">{{ item.title }}</nuxt-link>
+                </li>
+            </ul>
+            <div class="mt-4" v-html="processedContent"></div>
             <div class="mt-4 text-[#FF8200]">
                 <span>{{ formattedPublicationDate }}</span> | <span>{{ post.category_name }}</span>
             </div>
@@ -19,7 +25,11 @@ import axios from 'axios'
 export default {
     data() {
         return {
-            post: [],
+            post: null,
+            introductionContent: '',
+            processedContent: '',
+            summary: [],
+            formattedPublicationDate: ''
         };
     },
     created() {
@@ -27,8 +37,8 @@ export default {
 
         axios.get(`http://127.0.0.1:8080/post/post/${id}`)
             .then(response => {
-                console.log("Posts fetched:", response.data)
-                this.post = response.data.data;
+                const post = response.data.data;
+                this.post = post;
 
                 const publicationDate = new Date(response.data.publication_date)
                 this.formattedPublicationDate = publicationDate.toLocaleDateString("Pt-BR", {
@@ -37,10 +47,93 @@ export default {
                     month: "2-digit",
                     day: "2-digit",
                 });
+
+                this.parseContent(post.content)
             })
             .catch(error => {
                 console.error("Error fetching posts", error)
             })
     },
+    methods: {
+        parseContent(content) {
+            const lines = content.split('\n');
+            const newContent = [];
+            const introductionContent = [];
+            const titles = [];
+            let isInCodeBlock = false;
+            let codeBlockContent = [];
+            let isFirstTitleFound = false;
+
+            lines.forEach((line, index) => {
+                const isTitle = line.startsWith('<title>');
+                const isImage = line.includes('<image>') && line.includes('</image>');
+                const isCodeStart = line.startsWith('<code>')
+                const isCodeEnd = line.endsWith('</code>');
+                const IsList = line.startsWith('-');
+
+
+                if (!content) return;
+
+
+                if (isTitle) {
+                    isFirstTitleFound = true;
+                    const title = line.replace(/<title>/, '').trim();
+                    const formattedTitle = title.replace(/\s+/g, '-');
+                    titles.push({ title, formattedTitle });
+
+                    newContent.push(`<h2 id="${formattedTitle}" class="text-3xl font-bold text-primary">${title}</h2>`)
+                    return
+                }
+
+                if (!isFirstTitleFound) {
+                    if (isImage) {
+                        const imgSrc = line.replace(/<image>|<\/image>/g, '').trim();
+                        introductionContent.push(`<img class="w-full" src="${imgSrc}" alt="Introduction Image" />`);
+                        return
+                    }
+
+                    introductionContent.push(`<p>${line}</p><br />`)
+                    return
+                }
+
+                if (isCodeStart) {
+                    isInCodeBlock = true;
+
+                    codeBlockContent.push(line.replace(/<code>/, ''))
+                    return
+                }
+
+                if (isInCodeBlock) {
+                    if (isCodeEnd) {
+                        isInCodeBlock = false
+                        codeBlockContent.push(line.replace(/<\/code>/, ''))
+                        newContent.push(
+                            `<pre class="border border-primary p-4 rounded-lg bg-[#1E1E1E] text-white overflow-auto"><code>${codeBlockContent
+                                .join('\n')
+                                .replace(/</g, '&lt;')
+                                .replace(/>/g, '&gt;')}</code></pre>`
+                        );
+                        codeBlockContent = []
+                    } else {
+                        codeBlockContent.push(line)
+                    }
+
+                    return
+                }
+
+                if (IsList) {
+                    const listItem = line.slice(1).trim()
+                    newContent.push(`<li>${listItem}</li>`)
+                    return
+                }
+
+                newContent.push(`<p>${line}</p><br />`)
+            })
+
+            this.introductionContent = introductionContent.join('')
+            this.processedContent = newContent.join('')
+            this.summary = titles
+        }
+    }
 };
 </script>
