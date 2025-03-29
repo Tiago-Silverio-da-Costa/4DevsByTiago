421c9eae6171b3ae64e6d202da25b48acc122d8c
Tiago-Silverio-da-Costa <tiagosilveriodacosta@gmail.com>
2025-03-28 21:08:40 -0300

    feat: filter home + input search improved + post styles improved

--- components/Search.vue
+++ components/Search.vue
@@ -1,9 +1,13 @@
 <template>
-    <form class="flex justify-center items-center text-black w-full" @submit.prevent>
-        <input placeholder="Pesquisar" class="border px-3 py-2 rounded-tl-md rounded-bl-md w-full h-full outline-none" v-model="localQuery" @input="emitSearch" />
+    <form class="flex justify-center items-center bg-[#121212] rounded-md text-black w-full" @submit.prevent>
+        <input placeholder="Pesquisar" class="bg-[#121212] px-3 py-2 rounded-tl-md rounded-bl-md w-full h-full outline-none" v-model="localQuery" @input="emitSearch" />
 
-        <button type="button" class="flex items-center justify-center text-xl bg-[#FF8200] rounded-tr-md rounded-br-md px-6 py-2 h-full flex-shrink-0" @click="emitSearch">
-            <Icon name="ic:baseline-search" class="w-6 h-6" />
+        <button
+            type="button"
+            class="flex items-center justify-center text-xl bg-[#FF8200] rounded-tr-md rounded-br-md px-6 min-h-[2.55rem] outline-none active:bg-[#FF8200] h-full flex-shrink-0"
+            @click="emitSearch"
+        >
+            <Icon name="ic:baseline-search" class="bg-white w-6 h-6" />
         </button>
     </form>
 </template>
