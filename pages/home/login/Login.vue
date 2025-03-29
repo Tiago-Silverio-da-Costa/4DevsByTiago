47a46ad859fdd4746f4fa9e5dee1bc3dc90ce4d9
Tiago-Silverio-da-Costa <tiagosilveriodacosta@gmail.com>
2025-03-28 21:57:34 -0300

    feat: implementando validacao base login

--- pages/home/login/Login.vue
+++ pages/home/login/Login.vue
@@ -1,70 +1,139 @@
+<script setup>
+import {ref} from "vue";
+import axios from "axios";
+import {useForm, defineRule, configure} from "vee-validate";
+import * as yup from "yup";
+
+const schema = yup.object({
+    email: yup.string().email("Invalid email").required("Email is required"),
+    password: yup.string().min(6, "At least 6 characters").required("Password is required"),
+});
+
+const {defineField, handleSubmit, errors} = useForm({validationSchema: schema});
+
+const [email, emailAttrs] = defineField("email");
+const [password, passwrodAttrs] = defineField("password");
+
+const isLoading = ref(false);
+const errorMessage = ref("");
+const showPassword = ref(false);
+
+const togglePassword = () => {
+    showPassword.value = !showPassword.value;
+};
+
+const onSubmit = handleSubmit(async (values) => {
+    console.log(values);
+    isLoading.value = true;
+    errorMessage.value = "";
+
+    const dataSend = {
+        user: {
+            email: values.email,
+            password: values.password,
+        },
+    };
+
+    try {
+        const runtimeConfig = useRuntimeConfig();
+        const response = await axios.post(`${runtimeConfig.public.apiBase}/user/login`, dataSend);
+
+        localStorage.setItem("token", response.data.results.token);
+        localStorage.setItem("userId", response.data.results.userId);
+
+        navigateTo("/home");
+    } catch (error) {
+        errorMessage.value = error.response?.data?.message || "An error occured while logging in";
+    } finally {
+        isLoading.value = false;
+    }
+});
+
+// export default {
+//     data() {
+//         return {
+//             email: "",
+//             password: "",
+//             isLoading: false,
+//             errorMessage: "",
+//             showPassword: false,
+//         };
+//     },
+//     methods: {
+//         togglePassword() {
+//             this.showPassword = !this.showPassword;
+//         },
+//         async handleLogin() {
+//             this.isLoading = true;
+//             this.errorMessage = "";
+
+//             const dataSend = {
+//                 user: {
+//                     email: this.email,
+//                     password: this.password,
+//                 },
+//             };
+
+//             try {
+//                 const runtimeConfig = useRuntimeConfig();
+
+//                 const response = await axios.post(`${runtimeConfig.public.apiBase}/user/login`, dataSend);
+//                 localStorage.setItem("token", response.data.results.token);
+//                 localStorage.setItem("userId", response.data.results.userId);
+
+//                 this.$router.push("/");
+//             } catch (error) {
+//                 this.errorMessage = error.response?.data?.message || "An error occured while logging in.";
+//             } finally {
+//                 this.isLoading = false;
+//             }
+//         },
+//     },
+// };
+</script>
+
 <template>
     <section class="bg-[#181C14]">
         <div class="flex flex-col justify-center items-center gap-4 mx-auto w-5/6 max-w-xs py-8 h-screen">
             <NuxtImg src="/logo-no-name.svg" alt="image" width="100" height="200" />
-            <h1 class="text-white text-3xl font-bold">Login</h1>
-            <form @submit.prevent="handleLogin" class="w-full flex flex-col justify-center items-center mt-8">
-                <input v-model="email" type="email" class="w-full placeholder:pl-4 mt-4" placeholder="Email" required />
-                <input v-model="password" type="password" class="w-full placeholder:pl-4 mt-4" placeholder="Password" required />
+            <h1 class="text-white text-3xl font-bold">Log in</h1>
+            <form @submit="onSubmit" class="w-full flex flex-col justify-center items-center mt-8">
+                <div class="flex flex-col gap-4 w-full">
+                    <div class="flex flex-col">
+                        <input
+                            v-model="email"
+                            v-bind="emailAttrs"
+                            class="w-full placeholder:pl-4 rounded p-2 bg-white border-2 focus:outline-none"
+                            :class="errors.email ? 'border-red-500' : 'border-gray-300 focus:border-[#ff8200]'"
+                            placeholder="Email"
+                        />
+                        <p v-if="errors.email" class="text-red-500 text-sm mt-1">{{ errors.email }}</p>
+                    </div>
+                    <div class="flex flex-col">
+                        <div
+                            class="flex items-center gap-3 w-full rounded p-2 bg-white border-2 focus-within:outline-none"
+                            :class="errors.password ? 'border-red-500' : 'border-gray-300 focus-within:border-[#ff8200]'"
+                        >
+                            <input
+                                v-model="password"
+                                v-bind="passwordAttrs"
+                                :type="showPassword ? 'text' : 'password'"
+                                class="w-full outline-none placeholder-gray-400 bg-transparent"
+                                placeholder="Password"
+                            />
+
+                            <Icon v-if="!showPassword" @click="togglePassword" name="mdi:eye-outline" class="w-6 h-6 text-[#ff8200] cursor-pointer" />
+                            <Icon v-if="showPassword" @click="togglePassword" name="mdi:eye-off-outline" class="w-6 h-6 text-[#ff8200] cursor-pointer" />
+                        </div>
+                        <p v-if="errors.password" class="text-red-500 text-sm mt-1">{{ errors.password }}</p>
+                    </div>
+                </div>
+
                 <NuxtLink :to="`/home/forgotpassword`" class="text-[#FF8200] font-semibold text-sm mt-2">forgot password? </NuxtLink>
-                <button type="submit" class="w-full px-4 py-2 rounded-md bg-[#FF8200] font-bold mt-4" :disabled="isLoading">{{ isLoading ? "Loading..." : "Enter" }}</button>
+                <button type="submit" class="w-full px-4 py-2 rounded-md bg-[#FF8200] font-bold mt-4" :disabled="isLoading">{{ isLoading ? "Loading..." : "Log in " }}</button>
                 <NuxtLink :to="`/home/register`" class="text-[#FF8200] font-semibold text-sm mt-2">don't have an account? </NuxtLink>
             </form>
             <p v-if="errorMessage" class="text-red-500 mt-4">{{ errorMessage }}</p>
         </div>
     </section>
 </template>
-
-<script>
-import axios from "axios";
-
-export default {
-    data() {
-        return {
-            email: "",
-            password: "",
-            isLoading: false,
-            errorMessage: "",
-        };
-    },
-    methods: {
-        async handleLogin() {
-            this.isLoading = true;
-            this.errorMessage = "";
-
-            const dataSend = {
-                user: {
-                    email: this.email,
-                    password: this.password,
-                },
-            };
-
-            try {
-                const runtimeConfig = useRuntimeConfig();
-
-                const response = await axios.post(`${runtimeConfig.public.apiBase}/user/login`, dataSend);
-                localStorage.setItem("token", response.data.results.token);
-                localStorage.setItem("userId", response.data.results.userId);
-
-                this.$router.push("/");
-            } catch (error) {
-                this.errorMessage = error.response?.data?.message || "An error occured while logging in.";
-            } finally {
-                this.isLoading = false;
-            }
-        },
-    },
-};
-</script>
-
-<style scoped>
-input {
-    padding: 8px;
-    border: 1px solid #ccc;
-    border-radius: 4px;
-}
-input:focus {
-    outline: none;
-    border-color: #ff8200;
-}
-</style>
--- pages/home/register/Register.vue
+++ pages/home/register/Register.vue
@@ -4,9 +4,31 @@
             <NuxtImg src="/logo-no-name.svg" alt="image" width="100" height="200" />
             <h1 class="text-white text-3xl font-bold">Register</h1>
             <form @submit.prevent="handleSubmit" class="w-full flex flex-col justify-center items-center mt-8">
-                <input v-model="name" class="w-full placeholder:pl-4 mt-4" placeholder="Name" />
-                <input v-model="email" type="email" class="w-full placeholder:pl-4 mt-4" placeholder="Email" />
-                <input v-model="password" type="password" class="w-full placeholder:pl-4 mt-4" placeholder="Password" />
+                <div class="flex flex-col gap-4 w-full">
+                    <input
+                        v-model="name"
+                        class="w-full placeholder:pl-4 rounded p-2 bg-white border-2 border-gray-300 focus-within:border-[#ff8200] outline-none"
+                        placeholder="Name"
+                    />
+                    <input
+                        v-model="email"
+                        type="email"
+                        class="w-full placeholder:pl-4 rounded p-2 bg-white border-2 border-gray-300 focus-within:border-[#ff8200] outline-none"
+                        placeholder="Email"
+                    />
+                    <div class="flex items-center gap-3 w-full rounded p-2 bg-white border-2 border-gray-300 focus-within:border-[#ff8200]">
+                        <input
+                            v-model="password"
+                            :type="showPassword ? 'text' : 'password'"
+                            class="w-full outline-none placeholder-gray-400 bg-transparent"
+                            placeholder="Password"
+                            required
+                        />
+
+                        <Icon v-if="!showPassword" @click="togglePassword" name="mdi:eye-outline" class="w-6 h-6 text-[#ff8200] cursor-pointer" />
+                        <Icon v-if="showPassword" @click="togglePassword" name="mdi:eye-off-outline" class="w-6 h-6 text-[#ff8200] cursor-pointer" />
+                    </div>
+                </div>
                 <button class="w-full px-4 py-2 rounded-md bg-[#FF8200] font-bold mt-4">Register</button>
                 <NuxtLink :to="`/home/login`" class="text-[#FF8200] font-semibold text-sm mt-2">already have an account? </NuxtLink>
             </form>
@@ -23,9 +45,13 @@ export default {
             name: "",
             email: "",
             password: "",
+            showPassword: false,
         };
     },
     methods: {
+        togglePassword() {
+            this.showPassword = !this.showPassword;
+        },
         async handleSubmit() {
             try {
                 const runtimeConfig = useRuntimeConfig();
@@ -46,15 +72,3 @@ export default {
     },
 };
 </script>
-
-<style scoped>
-input {
-    padding: 8px;
-    border: 1px solid #ccc;
-    border-radius: 4px;
-}
-input:focus {
-    outline: none;
-    border-color: #ff8200;
-}
-</style>
